011f2d4fff3792c8ccd4c53276bef7bf9b7ebb0b
valros76 <valerian.dufrene@gmail.com>
2021-01-04 14:51:03 +0100

    Add result in html preview - Exercice 6 part 1

--- exercice6/public/sources/js/alert.js
+++ exercice6/public/sources/js/alert.js
@@ -1,9 +1,11 @@
 window.addEventListener("DOMContentLoaded", (e)=>{
+   var resultat = document.getElementById("resultat")
    var submit = document.getElementById('submit')
    submit.addEventListener("click", (e)=>{
+      e.preventDefault()
       var first = parseFloat(document.getElementById('firstNumber').value)
       var second = parseFloat(document.getElementById('secondNumber').value)
       var result = (first % second)
-      alert(`Résultat : ${result}`)
+      resultat.textContent = result
    })
 })
