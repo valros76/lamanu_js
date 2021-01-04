b59673c0fc7667898038995a422dcfd5aae911ba
valros76 <valerian.dufrene@gmail.com>
2021-01-04 14:46:31 +0100

    Transform js work on html form in exercice5

--- exercice5/public/sources/js/alert.js
+++ exercice5/public/sources/js/alert.js
@@ -1,6 +1,14 @@
 window.addEventListener("DOMContentLoaded", (e)=>{
-   var first = parseFloat(prompt('Entrez un nombre décimal : '))
-   var second = parseFloat(prompt('Entrez un deuxième nombre décimal : '))
+   let calc = (first,second)=>{
    var result = Math.trunc(first) * second
-   alert(`Résultat : ${result}`)
+   return result
+   }
+   var submit_btn = document.getElementById("submit")
+   submit_btn.addEventListener("click", (e) => {
+      e.preventDefault()
+      var result = document.getElementById("result")
+      var first = parseFloat(document.getElementById("first").value)
+      var second = parseFloat(document.getElementById("second").value)
+      result.textContent = calc(first,second)
+   })
 })
